a028afbc533b1c61c3f554407d6baefe2963f8a7
J. Andrew Thompson <jathom5@ilstu.edu>
2016-02-05 22:19:38 -0600

    refining Board functions

--- src/Board.purs
+++ src/Board.purs
@@ -3,17 +3,44 @@ module Board where
 import Prelude
 import Piece
 import Data.Maybe
-import Data.Array (replicate)
+import Data.Array
+import qualified Data.Map as M
+
+data Coord = Coord Int Int
 
 type Space = Maybe Piece
 
-type Board = Array (Array Space)
+type Board = M.Map Coord Piece
 
-emptyRow :: Array Space
-emptyRow = replicate 4 Nothing
-
+--emptyRow :: Array Space
+--emptyRow = replicate 4 Nothing
+--
 emptyBoard :: Board
-emptyBoard = replicate 4 emptyRow
+emptyBoard = M.empty
+
+setPiece :: Coord -> Piece -> Board -> Board
+setPiece = M.insert 
+
+getSpace :: Coord -> Board -> Space
+getSpace = M.lookup
+
+isSpaceAvailable :: Coord -> Board -> Boolean
+isSpaceAvailable c b = result where
+    space = getSpace c b
+    result = case space of
+                  Nothing -> true
+                  Just _  -> false
+
+
+
 
 instance showCoord :: Show Coord where
     show (Coord x y) = "Row: " ++ show x ++ ", Column: " ++ show y
+
+instance eqCoord :: Eq Coord where
+    eq (Coord x1 y1) (Coord x2 y2) = if x1 == x2 then eq y1 y2 else eq x1 x2
+
+instance compareCoord :: Ord Coord where
+    compare (Coord x1 y1) (Coord x2 y2) = if x1 == x2 then compare y1 y2 else compare x1 x2 
+                                        
+
--- src/Main.purs
+++ src/Main.purs
@@ -3,9 +3,25 @@ module Main where
 import Prelude
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
-import Data.Array (replicate, (!!))
 import Data.Maybe 
+import Piece
+import Board
 import DOM 
+
+data Player = P1 | P2
+
+type Game = { turn :: Player
+            , board :: Board }
+
+newGame ::  Game
+newGame = { turn: P1, board: emptyBoard }
+
+instance showPlayer :: Show Player where
+    show P1 = "P1"
+    show P2 = "P2"
+
+--instance showGame :: Show Game where
+--    show g = show g.turn ++ " :: " ++ show g.board
 
 --import Piece
    
